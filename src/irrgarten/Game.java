3fe676265302548f9fc6e740473368c383350a14
usuario <usuario@usuario-vb>
2025-03-28 14:00:45 +0100

    Practica 2 empezada

---
+++ src/irrgarten/Game.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author usuario
+ */
+public class Game {
+    private static final int MAX_ROUNDS=10;
+    
+    private int currentPlayerIndex;
+    private String log;
+    
+    public Game(int nplayers){
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean finished(){
+        throw new UnsupportedOperationException();
+    }
+    public boolean nexStep(Directions preferredDirection){
+        throw new UnsupportedOperationException();
+    }
+    public GameState getGameState(){
+        throw new UnsupportedOperationException();
+    }
+    private void configureLabyrinth(){
+        throw new UnsupportedOperationException();
+    }
+    private void nextPlayer(){
+        throw new UnsupportedOperationException();
+    }
+    private Directions actualDirection(Directions preferredDirection){
+        throw new UnsupportedOperationException();
+    }
+    private GameCharacter combat(Monster monster){
+        throw new UnsupportedOperationException();
+    }
+    private void manageReward(GameCharacter winner){
+        throw new UnsupportedOperationException();
+    }
+    private void manageResurrection(){
+        throw new UnsupportedOperationException();
+    }
+    private void logPlayerWon(){
+        throw new UnsupportedOperationException();
+    }
+    private void logMonsterWon(){
+        throw new UnsupportedOperationException();
+    }
+    private void logResurrected(){
+        throw new UnsupportedOperationException();
+    }
+    private void logPlayerSkipTurn(){
+        throw new UnsupportedOperationException();
+    }
+    private void logPlayerNoOrders(){
+        throw new UnsupportedOperationException();
+    }
+    private void logNoMonster(){
+        throw new UnsupportedOperationException();
+    }
+    private void logRounds(){
+        throw new UnsupportedOperationException();
+    }
+}
